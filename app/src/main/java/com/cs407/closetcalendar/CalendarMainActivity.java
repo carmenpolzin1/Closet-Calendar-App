86f9e638acd669b193af3b627d23df07a150dd4c
Jessica Xu <xuujessicaa@gmail.com>
2023-10-29 23:10:52 -0500

    added camerax (didn't add image analysis yet). had to refactor icon names and comment out because tools:sample was giving me issues. added button on main screen to go to camera activity

--- app/src/main/java/com/cs407/closetcalendar/CalendarMainActivity.java
+++ app/src/main/java/com/cs407/closetcalendar/CalendarMainActivity.java
@@ -2,7 +2,9 @@ package com.cs407.closetcalendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CalendarMainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class CalendarMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cal_main);
+    }
+
+    public void goToCamera(View view) {
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
     }
 }
